2d36d66e1ffc879a121b3bfea6f3690c673942a1
G. Allegretta <allegretta92@hotmail.it>
2022-06-26 17:05:46 +0200

    Ehnance form definition

--- projects/demo-without-dynamic-form/src/app/form.definition.ts
+++ projects/demo-without-dynamic-form/src/app/form.definition.ts
@@ -1,15 +1,25 @@
 import { FormControl } from '@angular/forms';
 
+export type colors = 'Red' | 'Blue' | 'Yellow';
+export type seasons = 'Spring' | 'Summer' | 'Fall' | 'Winter';
+export type musics = 'Classic' | 'Country' | 'Folk' | 'Rap' | 'Rock';
+export type foods = 'icecream-0' | 'pizza-1' | 'tacos-2';
+
+export interface Food {
+  value: foods,
+  viewValue: string
+}
+
 export interface FormLeftRepresentation {
   firstName: FormControl<string | null>
   lastName: FormControl<string | null>
-  faveFood: FormControl<string | null>
-  faveColor: FormControl<string | null>
+  faveFood: FormControl<foods | null>
+  faveColor: FormControl<colors | null>
 }
 
 export interface FormRightRepresentation {
   phone: FormControl<string | null>
   email: FormControl<string | null>
-  faveSeason: FormControl<string | null>
-  faveMusic: FormControl<string | null>
+  faveSeason: FormControl<seasons | null>
+  faveMusic: FormControl<musics | null>
 }
--- projects/demo-without-dynamic-form/src/app/new-form/new-form.component.ts
+++ projects/demo-without-dynamic-form/src/app/new-form/new-form.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-import { FormLeftRepresentation, FormRightRepresentation } from '../form.definition';
+import { colors, Food, FormLeftRepresentation, FormRightRepresentation, musics, seasons } from '../form.definition';
 
 @Component({
   selector: 'app-new-form',
@@ -9,27 +9,27 @@ import { FormLeftRepresentation, FormRightRepresentation } from '../form.definit
 })
 export class NewFormComponent implements OnInit {
   formLeft = new FormGroup<FormLeftRepresentation>({
-    firstName: new FormControl(''),
-    lastName: new FormControl(''),
-    faveFood: new FormControl(''),
-    faveColor: new FormControl('')
+    firstName: new FormControl(),
+    lastName: new FormControl(),
+    faveFood: new FormControl(),
+    faveColor: new FormControl()
   });
 
   formRight = new FormGroup<FormRightRepresentation>({
-    phone: new FormControl(''),
-    email: new FormControl(''),
-    faveSeason: new FormControl(''),
-    faveMusic: new FormControl('')
+    phone: new FormControl(),
+    email: new FormControl(),
+    faveSeason: new FormControl(),
+    faveMusic: new FormControl()
   });
 
-  foods = [
+  foods: Food[] = [
     {value: 'icecream-0', viewValue: 'Ice Cream'},
     {value: 'pizza-1', viewValue: 'Pizza'},
     {value: 'tacos-2', viewValue: 'Tacos'}
   ];
-  colors = ['Red', 'Blue', 'Yellow'];
-  seasons = ['Spring', 'Summer', 'Fall', 'Winter'];
-  musics = ['Classic', 'Country', 'Folk', 'Rap', 'Rock'];
+  colors: colors[] = ['Red', 'Blue', 'Yellow'];
+  seasons: seasons[] = ['Spring', 'Summer', 'Fall', 'Winter'];
+  musics: musics[] = ['Classic', 'Country', 'Folk', 'Rap', 'Rock'];
 
   ngOnInit(): void {
     this.formLeft.valueChanges.subscribe(values => {
