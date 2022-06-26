8429bc3f3889acf9183b259b2f6a97ab21ea4f31
G. Allegretta <allegretta92@hotmail.it>
2022-06-26 17:07:58 +0200

    Remove UntypedFormControl and UntypedFormGroup
    from projects/demo-without-dynamic-form/src/app/edit-form/edit-form.component.ts

--- projects/demo-without-dynamic-form/src/app/edit-form/edit-form.component.ts
+++ projects/demo-without-dynamic-form/src/app/edit-form/edit-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { UntypedFormControl, UntypedFormGroup } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
+import { colors, Food, FormLeftRepresentation, FormRightRepresentation, musics, seasons } from '../form.definition';
 
 @Component({
   selector: 'app-edit-form',
@@ -7,28 +8,28 @@ import { UntypedFormControl, UntypedFormGroup } from '@angular/forms';
   styleUrls: ['./edit-form.component.scss']
 })
 export class EditFormComponent implements OnInit {
-  formLeft = new UntypedFormGroup({
-    firstName: new UntypedFormControl('Mickey'),
-    lastName: new UntypedFormControl('Mouse'),
-    faveFood: new UntypedFormControl('pizza-1'),
-    faveColor: new UntypedFormControl('Blue')
+  formLeft = new FormGroup<FormLeftRepresentation>({
+    firstName: new FormControl('Mickey'),
+    lastName: new FormControl('Mouse'),
+    faveFood: new FormControl('pizza-1'),
+    faveColor: new FormControl('Blue')
   });
 
-  formRight = new UntypedFormGroup({
-    phone: new UntypedFormControl('123-456-1212'),
-    email: new UntypedFormControl('mickey@disney.com'),
-    faveSeason: new UntypedFormControl('Summer'),
-    faveMusic: new UntypedFormControl('Rock')
+  formRight = new FormGroup<FormRightRepresentation>({
+    phone: new FormControl('123-456-1212'),
+    email: new FormControl('mickey@disney.com'),
+    faveSeason: new FormControl('Summer'),
+    faveMusic: new FormControl('Rock')
   });
 
-  foods = [
+  foods: Food[] = [
     {value: 'icecream-0', viewValue: 'Ice Cream'},
     {value: 'pizza-1', viewValue: 'Pizza'},
     {value: 'tacos-2', viewValue: 'Tacos'}
   ];
-  colors = ['Red', 'Blue', 'Yellow'];
-  seasons = ['Spring', 'Summer', 'Fall', 'Winter'];
-  musics = ['Classical', 'Country', 'Folk', 'Rap', 'Rock'];
+  colors: colors[] = ['Red', 'Blue', 'Yellow'];
+  seasons: seasons[] = ['Spring', 'Summer', 'Fall', 'Winter'];
+  musics: musics[] = ['Classic', 'Country', 'Folk', 'Rap', 'Rock'];
 
   ngOnInit(): void {
     this.formLeft.valueChanges.subscribe(values => {
